ca40ccbb4887266af82d356a931877a5ff6cf0e1
Dulche <115841388+kevin-dulche@users.noreply.github.com>
2024-03-18 05:19:14 +0000

    Agregamos la semana 00

---
+++ 00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/00.py
@@ -0,0 +1,10 @@
+# https://www.python.org/
+'''
+    https://www.python.org/
+'''
+variable = "Hola Mundo"
+variableEntero = 10
+variableFlotante = 10.5
+variableBooleana = True
+
+print("¡Hola, python!")
